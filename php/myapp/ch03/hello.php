8a97f2e786e537c85cf6a8031ae106b292111f61
Kim Jae Hyun <84066112+takingtheback@users.noreply.github.com>
2022-06-17 15:42:37 +0900

    ch3 - Hello PHP

---
+++ php/myapp/ch03/hello.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "Hello PHP!"
+    ?>
+<div class="php">
+    PHP 명령어로 작성한 <?=$str ?>
+</div>
+<div class="html">
+    HTML 태그로 작성한 Hello PHP!
+</div>
+
+</body>
+</html>
+

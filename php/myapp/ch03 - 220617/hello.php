73e59f469fd26b3f4cc7b99385f39a12e3d462cb
Kim Jae Hyun <84066112+takingtheback@users.noreply.github.com>
2022-06-17 16:21:41 +0900

    php 220617

---
+++ php/myapp/ch03 - 220617/hello.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "Hello PHP!";
+        $num = 1;
+    ?>
+    <div class="php">
+        PHP 명령어로 작성한 <?=$str ?>
+    </div>
+    <div class="php">
+        PHP 명령어로 작성한 <?=$num ?>
+        인터프리터란? 컴파일과 반대로 컴파일 작업을 하지 않고 소스를 실행할 때마다 해석하여 기계어로...
+    </div>
+    <div class="html">
+        HTML 태그로 작성한 Hello PHP!
+    </div>
+</body>
+</html>
+
